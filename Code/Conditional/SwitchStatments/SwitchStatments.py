07ec8e7d2370f3c61daf39a32224024e088c1d82
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-16 12:54:24 +0800

    SwitchStatments

---
+++ Code/Conditional/SwitchStatments/SwitchStatments.py
@@ -0,0 +1,12 @@
+switch = dict(
+              one = 'one',
+              two = 'two',
+              three = 'three'
+              )
+
+var = 'one'
+
+print(switch[var])
+print(switch.get(var,'default'))
+
+print(switch['three'])
