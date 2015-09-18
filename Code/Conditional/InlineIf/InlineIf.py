1f3d342025ef44c8839da1e81e5acd72d87d99b3
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-18 09:48:48 +0800

    InlineIf

---
+++ Code/Conditional/InlineIf/InlineIf.py
@@ -0,0 +1,13 @@
+a,b = 1,0
+
+if a == b:
+    print(True)
+else:
+    print(False)
+    
+print(True if a==b else False)
+print('this is true' if a==b else 'this is not true')
+
+var = 'this is true' if a==b else 'this is not true'
+print(var)
+
