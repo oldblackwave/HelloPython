fa979be2c86df08a4e1a645bc5544e632009b39d
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-18 10:11:57 +0800

    Looping

---
+++ Code/Looping/ForLoops/ForLoops.py
@@ -0,0 +1,10 @@
+for data in [1,2,3,4,5]:
+    print(data)
+    
+for data in 'string':
+    print(data)
+    
+for key,data in enumerate('string'):
+    if key %2 == 0:
+        print(data)
+    
---
+++ Code/Looping/TryExceptFinally/TryExceptFinally.py
@@ -0,0 +1,18 @@
+tuple = (1,2,3,4,5)
+
+try:
+    tuple.append(6)
+except AttributeError as e:
+    print('error', e)
+else:
+    for each in tuple:
+        print(each)
+    
+
+try:
+    tuple.append(6)
+    for each in tuple:
+        print(each)
+except AttributeError as e:
+    print('error', e)
+    
