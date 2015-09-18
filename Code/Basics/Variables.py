979c661b58388a6fc8dfd075a4782349cfe95eb8
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-18 12:26:38 +0800

    Variables

---
+++ Code/Basics/Variables.py
@@ -0,0 +1,13 @@
+print("variables test~~");
+
+firstnumber = 555;
+secondNumber = 888;
+
+print(firstnumber + secondNumber);
+
+thirdNumber = firstnumber + secondNumber;
+
+print(thirdNumber);
+print(id(thirdNumber));
+print(type(thirdNumber));
+
